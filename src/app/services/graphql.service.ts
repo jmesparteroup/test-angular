897664be3bf8703142386494cf9f4e0ac2879845
Joshua Espartero <jmespartero@up.edu.ph>
2023-04-22 23:24:20 +0800

    init

--- src/app/services/graphql.service.ts
+++ src/app/services/graphql.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink, ApolloClient, InMemoryCache } from '@apollo/client/core';
 import gql from 'graphql-tag';
 
 @Injectable({
@@ -11,9 +10,9 @@ export class GraphqlService {
   private endpoint = 'http://localhost:3000/graphql';
 
   constructor(private apollo: Apollo, private httpLink: HttpLink) {
-    const http = httpLink.create({ uri: this.endpoint });
+    const http = new HttpLink({ uri: this.endpoint });
 
-    this.apollo.create({
+    new ApolloClient({
       link: http,
       cache: new InMemoryCache(),
       connectToDevTools: true
